3fab0bb129c5f7f9e456e01adcad959928d352fe
ykisana <ykisana@users.noreply.github.com>
2021-03-23 20:32:10 -0600

    faster burrowswheeler

--- burrowswheeler.cpp
+++ burrowswheeler.cpp
@@ -7,7 +7,7 @@
 //  and problem solving, but you are not permitted to hunt down solutions to
 //  these particular problems!
 //
-//  SuffixArray from GeeksforGeeks: https://www.geeksforgeeks.org/suffix-array-set-1-introduction/
+//  SuffixArray from GeeksforGeeks: https://www.geeksforgeeks.org/suffix-array-set-2-a-nlognlogn-algorithm/
 //  Burrows Wheeler Transform Explanation: https://www.youtube.com/watch?v=4n7NPk5lwbI
 //  Further Burrows Wheeler Explanation/Optimization: https://en.wikipedia.org/wiki/Burrows%E2%80%93Wheeler_transform
 //
@@ -26,38 +26,85 @@
 
 using namespace std;
 
-// Structure to store information of a suffix
 struct suffix
 {
-    int index;
-    char *suff;
+    int index; // To store original index
+    int rank[2]; // To store ranks and next rank pair
 };
 
 // A comparison function used by sort() to compare two suffixes
+// Compares two pairs, returns 1 if first pair is smaller
 int cmp(struct suffix a, struct suffix b)
 {
-    return strcmp(a.suff, b.suff) < 0? 1 : 0;
+    return (a.rank[0] == b.rank[0])? (a.rank[1] < b.rank[1] ?1: 0):
+           (a.rank[0] < b.rank[0] ?1: 0);
 }
 
 // This is the main function that takes a string 'txt' of size n as an
 // argument, builds and return the suffix array for the given string
-int *buildSuffixArray(char *txt, int n)
+int *buildSuffixArray(string txt, int n)
 {
     // A structure to store suffixes and their indexes
     struct suffix suffixes[n];
 
     // Store suffixes and their indexes in an array of structures.
-    // The structure is needed to sort the suffixes alphabetically
+    // The structure is needed to sort the suffixes alphabatically
     // and maintain their old indexes while sorting
     for (int i = 0; i < n; i++)
     {
         suffixes[i].index = i;
-        suffixes[i].suff = (txt+i);
+        suffixes[i].rank[0] = txt[i] - 'a';
+        suffixes[i].rank[1] = ((i+1) < n)? (txt[i + 1] - 'a'): -1;
     }
 
     // Sort the suffixes using the comparison function
     // defined above.
     sort(suffixes, suffixes+n, cmp);
+
+    // At this point, all suffixes are sorted according to first
+    // 2 characters.  Let us sort suffixes according to first 4
+    // characters, then first 8 and so on
+    int ind[n];  // This array is needed to get the index in suffixes[]
+    // from original index.  This mapping is needed to get
+    // next suffix.
+    for (int k = 4; k < 2*n; k = k*2)
+    {
+        // Assigning rank and index values to first suffix
+        int rank = 0;
+        int prev_rank = suffixes[0].rank[0];
+        suffixes[0].rank[0] = rank;
+        ind[suffixes[0].index] = 0;
+
+        // Assigning rank to suffixes
+        for (int i = 1; i < n; i++)
+        {
+            // If first rank and next ranks are same as that of previous
+            // suffix in array, assign the same new rank to this suffix
+            if (suffixes[i].rank[0] == prev_rank &&
+                suffixes[i].rank[1] == suffixes[i-1].rank[1])
+            {
+                prev_rank = suffixes[i].rank[0];
+                suffixes[i].rank[0] = rank;
+            }
+            else // Otherwise increment rank and assign
+            {
+                prev_rank = suffixes[i].rank[0];
+                suffixes[i].rank[0] = ++rank;
+            }
+            ind[suffixes[i].index] = i;
+        }
+
+        // Assign next rank to every suffix
+        for (int i = 0; i < n; i++)
+        {
+            int nextindex = suffixes[i].index + k/2;
+            suffixes[i].rank[1] = (nextindex < n)?
+                                  suffixes[ind[nextindex]].rank[0]: -1;
+        }
+
+        // Sort the suffixes according to first k characters
+        sort(suffixes, suffixes+n, cmp);
+    }
 
     // Store indexes of all sorted suffixes in the suffix array
     int *suffixArr = new int[n];
@@ -74,29 +121,27 @@ int main(){
     //ios_base::sync_with_stdio(false); // this stops synchronizing I/O between iostream and stdio, avoiding some overhead
     //cin.tie(NULL); // ordinarily cin is tied to cout, meaning cin will flush cout before reading
     string s;
-    char chars[210000];
-    while(cin.getline(chars,210000)) {
-        int size = strlen(chars);
+    while(getline(cin,s)) {
+        //int size = s.length();
+        int size = s.length();
+
+        s = s + s;
+        int size2 = s.size();
         //cout << size;
-        //char chars2[size];
-        //strcpy(chars2,s.c_str());
-        //s = s + s;
-        //strcpy(chars,s.c_str());
-        strcat(chars,chars);
-        for (int i = size; i < size*2; ++i) {
-            chars[i] = chars[i-size];
-        }
+
+        //cout << s << '\n';
         //cout << chars;
 
-        int *suffixArr = buildSuffixArray(chars,  size);
+        int *suffixArr = buildSuffixArray(s, size2);
 
-        for (int i = 0; i < size; i++) {
-            if (suffixArr[i] > 0){
-                cout << chars[suffixArr[i]-1];
-
+        for (int i = 0; i < size2; i++) {
+            if (suffixArr[i] > 0) {
+                if(suffixArr[i] < size){
+                    cout << s[suffixArr[i] - 1];
+                }
             }
-            else{
-                cout << chars[size-1];
+            else {
+                cout << s[size - 1];
 
             }
         }
@@ -107,3 +152,4 @@ int main(){
 
     }
 }
+
